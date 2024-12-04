df744099408c06e7190622ecdaba77655676808e
luis-pc\luis <vluis5457@gmail.com>
2024-12-03 19:25:11 -0600

    Added data types

--- Variables_TiposDatos/Program.cs
+++ Variables_TiposDatos/Program.cs
@@ -1,10 +1,29 @@
-﻿class Program
+﻿using System;
+
+
+class Program
 {
     public static void Main(string[] arg)
     {
+        // DATA TYPES
 
 
-        Console.WriteLine("Presiona cualquier tecla para continuar...");
+        // INTEGER
+        int age = 10;
+        
+        Console.WriteLine($"Age: {age}");
+
+        // Change age value 
+        age = 30;
+
+        Console.WriteLine($"New age: {age}");
+
+        // STRING 
+        string name = "Luis Vasquez";
+
+        Console.WriteLine($"Name: {name}");
+
+        Console.WriteLine("Press any key to continue...");
         Console.ReadKey();
     }
 }
