c58da8f783ae63a9c35b0449eaade33b0c757836
luis-pc\luis <vluis5457@gmail.com>
2024-12-03 19:30:39 -0600

    Added comments project

---
+++ Comments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Comments");
+
+        // This is a single line comment
+        /*
+            This is a multi-line comment
+        */
+
+        int age = 25; // This is a single line comment
+        Console.WriteLine("Age: " + age);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
